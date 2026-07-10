774fdbcca4bee7d5ee6b063706a75ae66f025ad3
Vikas Kumar <8273vikaskumar@gmail.com>
2026-07-10 12:10:19 +0530

    Added SH001 Configuration Framework

---
+++ firmware/SH001-Smart-Core/src/config.h
@@ -0,0 +1,27 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+//==================================================
+// SH001 Smart Core Controller
+// Configuration File
+//==================================================
+
+// Device Information
+
+#define DEVICE_NAME "SH001"
+#define FIRMWARE_VERSION "0.1"
+
+// Status LED
+
+#define STATUS_LED 2
+
+// WiFi
+
+#define WIFI_SSID ""
+#define WIFI_PASSWORD ""
+
+// Serial
+
+#define SERIAL_BAUDRATE 115200
+
+#endif
---
+++ firmware/SH001-Smart-Core/src/gpio.cpp
@@ -0,0 +1,7 @@
+#include "gpio.h"
+#include "config.h"
+
+void initGPIO()
+{
+    pinMode(STATUS_LED, OUTPUT);
+}
